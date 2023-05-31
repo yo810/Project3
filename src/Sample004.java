6c015e0700bcc88d20da7d038604382b8a6200ff
mls80 <mls80@DESKTOP-EROOIJ1>
2023-05-31 18:48:06 +0900

    commit2 偶数のカウント追加

--- src/Sample004.java
+++ src/Sample004.java
@@ -1,12 +1,18 @@
 public class Sample004 {
 	public static void main(String[] args) {
 		int sum = 0;
+//		下記追加　カウントの変数
+		int cnt = 0;
 		for(int i = 1; i<= 10; i++) {
 			if(i % 2 == 0) {
 				sum += i;
+//				下記追加　カウントを+1
+				cnt ++;
 			}
-		}	
+		}
 		System.out.println("1～10までの偶数の合計は" + sum);
+//		下記追加　1～10までの表示
+		System.out.println("1～10までの偶数の個数は" + cnt);
 	}
 }
 

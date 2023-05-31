73ca55c02fe4afbfe957eec03cbad8c45d818f32
mls80 <mls80@DESKTOP-EROOIJ1>
2023-06-01 00:04:59 +0900

    commit4

--- src/Sample005.java
+++ src/Sample005.java
@@ -6,6 +6,7 @@ public class Sample005 {
         int[] number = {1,2,3,4,5,6,7,8,9,10};
 		for (int i = 0; i < number.length; i++) {
 			System.out.print(number[i] + ",");
+//			printlnメソッドは改行できる、printメソッドは改行しない
 		}
 	}
 }

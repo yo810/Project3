69d1044d37426fe43eba12ee72504c2908173f5b
mls80 <mls80@DESKTOP-EROOIJ1>
2023-06-02 03:12:24 +0900

    commit5　10にカンマをつけない

--- src/Sample005.java
+++ src/Sample005.java
@@ -5,8 +5,16 @@ public class Sample005 {
 	public static void main (String[] args) {
         int[] number = {1,2,3,4,5,6,7,8,9,10};
 		for (int i = 0; i < number.length; i++) {
-			System.out.print(number[i] + ",");
+	
+			System.out.print(number[i]); //数字を出力する	
+
+			if (number[i] != 10) //if文で10以外とする
+				
+				System.out.print(","); //10以外にカンマをつける
+
+
 //			printlnメソッドは改行できる、printメソッドは改行しない
+			
 		}
 	}
 }

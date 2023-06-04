6d85e6a293b67f0b8b8d81c14b4e54498d9297a7
mls80 <mls80@DESKTOP-EROOIJ1>
2023-06-05 05:40:51 +0900

    commit7

--- src/Sample005.java
+++ src/Sample005.java
@@ -1,22 +1,18 @@
 //1〜10までの整数をカンマ区切りで横並びで表示させる
 //(10にはカンマをつけない)
 
+
 public class Sample005 {
 	public static void main (String[] args) {
         int[] number = {1,2,3,4,5,6,7,8,9,10};
 		for (int i = 0; i < number.length; i++) {
-	
-			System.out.print(number[i]); //数字を出力する	
-
-			if (number[i] != 10) //if文で10以外とする
-				
-				System.out.print(","); //10以外にカンマをつける
-
-
-//			printlnメソッドは改行できる、printメソッドは改行しない
-			
+            if(i < number.length-1){
+			    System.out.print(number[i] + ",");
+            }else{
+			    System.out.print(number[i]);
+            }
 		}
 	}
 }
 
-//10の後のカンマをつけないがわからない
+//printlnメソッドは改行できる、printメソッドは改行しない
--- src/Sample006.java
+++ src/Sample006.java
@@ -1,23 +1,32 @@
+//要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる。[0]~[10]の値の
+//奇数のみカンマ区切りで、[11]~[19]の値は偶数をカンマ区切りで表示させる
+
 public class Sample006 {
 	public static void main (String[] args) {
 		int number[] = new int[20]; //20の要素を作成
+		String str ="";
+		number[0] = 0;
 		for(int i = 1; i < number.length; i++) { 
 			number[i] = number[i - 1] + 5; //0から5ずつnumber[i]に加算
 		}
-		for (int i = 0; i < number.length; i++) { 
-			System.out.println(String.format("%2d %4d", i, number[i]));
+		
+		System.out.print("\n奇数 : ");
+		for (int i = 0; i < 11; i++) {
+			if (number[i] % 2 != 0) {
+				str += String.valueOf(number[i]) + ",";
+			}
 		}
 		
-		for(int i = 0; i <= 10; i++) { //0～10までのループ
-			if(i % 2 != 0) //奇数
-				System.out.print("," +i); //奇数とカンマの表示
+		System.out.print(str.substring(0, str.length() - 1));
+		System.out.print("\n偶数 : ");
+		str = "";
+		for (int i = 11; i < 20; i++) {
+			if (number[i] % 2 == 0) {
+				str += String.valueOf(number[i]) + ",";
+			}
 		}
+		System.out.print(str.substring(0, str.length() - 1));
+		System.out.print("\n");
 		
-		System.out.println(); //改行
-
-		for(int i = 11; i <= 19; i++) { //11～19までのループ
-			if(i % 2 == 0) //偶数
-				System.out.print("," +i); //偶数とカンマの表示
-		}
 	}
 }
